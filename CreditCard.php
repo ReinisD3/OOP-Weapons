30e9844d002c3403455088d10b2695090a40260c
ReinisD3 <reinis.detlavs.com>
2021-09-15 11:38:01 +0300

    Update Code

---
+++ CreditCard.php
@@ -0,0 +1,36 @@
+<?php
+
+class CreditCard
+{
+    private string $nameSurname;
+    private string $cardNumber;
+    private string $cardCCV;
+
+    public function __toString(): string
+    {
+        return 'Credit Card';
+    }
+
+    public function getNameSurname(): string
+    {
+        return $this->nameSurname;
+    }
+
+    public function getCardNumber(): string
+    {
+        return $this->cardNumber;
+    }
+
+    public function getCardCCV(): string
+    {
+        return $this->cardCCV;
+    }
+
+    public function enterCredentials(): void
+    {
+        $this->nameSurname = trim(readline('Enter your Name Surname : ')) . PHP_EOL;
+        $this->cardNumber = trim(readline('Enter credit card number : ')) . PHP_EOL;
+        $this->cardCCV = trim(readline('Enter credit card CCV : ')) . PHP_EOL;
+    }
+
+}
---
+++ Paypal.php
@@ -0,0 +1,27 @@
+<?php
+
+class Paypal
+{
+    private string $address;
+    private string $password;
+
+    public function __toString(): string
+    {
+        return 'Paypal';
+    }
+
+    public function getAddress(): string
+    {
+        return $this->address;
+    }
+
+    public function getPassword(): string
+    {
+        return $this->password;
+    }
+    public function enterCredentials(): void
+    {
+        $this->address = trim(readline('Enter Paypal address : ')) . PHP_EOL;
+        $this->password = trim(readline('Enter Paypal password : ')) . PHP_EOL;;
+    }
+}
